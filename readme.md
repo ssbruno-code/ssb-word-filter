ab750c1c7999caa8b797a307c33af9a05b51d46b
ssbruno-code <ti.brunosantana@gmail.com>
2022-01-19 19:20:55 -0300

    First commit of the project

---
+++ readme.md
@@ -0,0 +1,20 @@
+## Simples Plugin para contagem de caracteres, palavras e prever o tempo de leitura do post.
+
+### ☕ Esse código é livre pra qualquer tipo de uso.
+
+#### O objetivo ao fazer este plugin era consolidar meus conhecimentos sobre a Options API do wordpress, salvando dados dentro do banco de dados.
+
+#### Qualquer contribuição é bem-vinda.
+
+#### Informaçoes: https://ssbruno.com
+
+## English
+## Simple Plugin for counting characters, words and predicting post reading time.
+
+### ☕ This code is free for any kind of use.
+
+#### The purpose of making this plugin was to consolidate my knowledge about the WordPress Options API, saving data inside the database.
+
+#### Any contribution is welcome.
+
+#### Information: https://ssbruno.com
---
+++ ssb-word-filter.php
@@ -0,0 +1,124 @@
+<?php 
+/*
+    Plugin Name: SSB Word Filter
+    Description: Plugin that filter words from posts and replace it with a word chosen by the user.
+    Version:1.0
+    Author: Bruno Santana Stefani
+    Author URI: https://ssbruno.com 
+    Text Domain: ssbwfdomain
+    Domain Path: /languages
+*/
+
+if(! defined( 'ABSPATH' )) exit;
+
+class SsbWordFilter{
+  function __construct(){
+
+    add_action('admin_menu', array($this, 'theMenu'));
+    add_action( 'admin_init', array($this, 'ssbSettings'));
+    
+    if(get_option( 'plugin_words_to_filter')) add_filter('the_content', array($this, 'filterLogic'));
+
+  }
+
+  function ssbSettings(){
+    add_settings_section( 'replacement-text-section', null, null, 'ssb-word-filter-options');
+    register_setting('replacementFields', 'replacementText');
+    add_settings_field( 'replacement-text', 'Filtered Text', array($this, 'replacementFieldHTML'), 'ssb-word-filter-options', 'replacement-text-section' );
+  }
+
+  function filterLogic($content){
+
+    $badWords = explode(',', get_option('plugin_words_to_filter'));
+    $badWordsTrimmed = array_map('trim', $badWords);
+    return str_ireplace($badWordsTrimmed, esc_html(get_option('replacementText', '***')), $content);
+
+  }
+  
+  function theMenu(){
+
+    $mainPageHook = add_menu_page('Words to Filter', 'SSB Word Filter', 'manage_options', 'ssb-word-filter', array($this, 'wordFilterPage'), 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+', 100);
+    add_submenu_page( 'ssb-word-filter', 'Words to Filter', 'Word List', 'manage_options', 'ssb-word-filter', array($this, 'wordFilterPage'));
+    add_submenu_page( 'ssb-word-filter', 'Word Filter Options', 'Options', 'manage_options', 'ssb-word-filter-options', array($this, 'optionsSubPage'));
+    add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
+    
+
+  }
+
+  function mainPageAssets(){
+
+    wp_enqueue_style( 'filterAdminCss', plugin_dir_url(__FILE__) . 'styles.css' );
+
+  }
+
+  function handleForm(){
+
+    if(wp_verify_nonce( $_POST['ourNonce'], 'saveFilterWords' ) AND current_user_can('manage_options')){
+
+      update_option('plugin_words_to_filter', sanitize_text_field($_POST['plugin_words_to_filter'])); ?>
+      <div class="updated">
+        <p>Your filtered words were saved.</p>
+      </div>
+      
+    <?php 
+    }else{ 
+    ?>
+
+      <div class="error">
+        <p>Sorry, you don't have permission to perform that action.</p>
+      </div>
+
+    <?php
+    }
+
+  }
+
+  function wordFilterPage(){ ?>
+
+    <div class="wrap">
+      <h1> Word Filter</h1>
+      <form method="POST">
+        <?php if($_POST['justsubmitted'] == "true") $this->handleForm() ?>
+        <input type="hidden" name="justsubmitted" value="true">
+        <?php
+          wp_nonce_field('saveFilterWords', 'ourNonce')
+        ?>
+        <label for="plugin_words_to_filter"><p>Enter a <strong>comma-separated</strong> list of words to filter from your site's content.</p></label>
+        <div class="word_filter__flex_container">
+          <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="Example: Bad, mean, awful, horrible..."><?php
+          echo esc_textarea(get_option('plugin_words_to_filter'));
+          ?></textarea>
+        </div>
+        <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+      </form>
+    </div>
+
+  <?php }
+
+function replacementFieldHTML(){ ?>
+
+  <input type="text" name="replacementText" value="<?php echo esc_attr(get_option('replacementText', '***')) ?>">
+  <p>Leave blank to simply remove the filtered words.</p>
+
+<?php }
+
+function optionsSubPage(){ ?>
+
+  <div class="wrap">
+    <h1>Word Filter Options</h1>
+    <form action="options.php" method="POST">
+      <?php
+      settings_errors();
+      settings_fields( 'replacementFields' );
+      do_settings_sections( 'ssb-word-filter-options' );
+      submit_button();
+      ?>
+    </form>
+  </div>
+
+<?php }
+
+
+}
+
+$ssbWordFilter = new SsbWordFilter();
